513f9e93bac60a7f9ef1e24d1d01b247bcd57ac8
Christoph Bühler <christoph@smartive.ch>
2023-08-20 12:29:28 +0200

    tests for dialog builder

--- test/builder/dialog_builder_test.dart
+++ test/builder/dialog_builder_test.dart
@@ -1,6 +1,9 @@
 import 'package:build/build.dart';
 import 'package:build_test/build_test.dart';
+import 'package:fluorflow/annotations.dart';
 import 'package:fluorflow_generator/src/builder/dialog_builder.dart';
+import 'package:recase/recase.dart';
+import 'package:source_gen/source_gen.dart';
 import 'package:test/test.dart';
 
 void main() {
@@ -653,11 +656,229 @@ extension Dialogs on _i1.DialogService {
     });
 
     group('with @DialogConfig()', () {
-      // TODO
+      test(
+          'should generate dialog method with custom default barrier color.',
+          () async => await testBuilder(
+              DialogBuilder(BuilderOptions.empty),
+              {
+                'a|lib/a.dart': '''
+                import 'package:fluorflow/annotations.dart';
+                import 'package:fluorflow/fluorflow.dart';
+
+                @DialogConfig(
+                  defaultBarrierColor: 0x34ff0000,
+                )
+                class MyDialog extends FluorFlowSimpleDialog {
+                  const MyDialog({super.key, required this.completer});
+                }
+              '''
+              },
+              outputs: {
+                'a|lib/app.dialogs.dart': r'''
+// ignore_for_file: type=lint
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'dart:ui' as _i2;
+
+import 'package:a/a.dart' as _i3;
+import 'package:fluorflow/fluorflow.dart' as _i1;
+
+extension Dialogs on _i1.DialogService {
+  Future<(bool?, dynamic)> showMyDialog(
+          {_i2.Color barrierColor = const _i2.Color(0x34ff0000)}) =>
+      showDialog<(bool?, dynamic)>(
+        barrierColor: barrierColor,
+        dialogBuilder: _i1.NoTransitionPageRouteBuilder(
+            pageBuilder: (
+          _,
+          __,
+          ___,
+        ) =>
+                _i3.MyDialog(completer: closeDialog)),
+      ).then((r) => (r?.$1, r?.$2));
+}
+'''
+              },
+              reader: await PackageAssetReader.currentIsolate()));
+
+      test(
+          'should generate dialog method with custom page builder.',
+          () async => await testBuilder(
+              DialogBuilder(BuilderOptions.empty),
+              {
+                'a|lib/a.dart': '''
+                import 'package:fluorflow/annotations.dart';
+                import 'package:fluorflow/fluorflow.dart';
+               
+                import 'b.dart';
+
+                @DialogConfig(
+                  routeBuilder: RouteBuilder.custom,
+                  pageRouteBuilder: CustomBuilder,
+                )
+                class MyDialog extends FluorFlowSimpleDialog {
+                  const MyDialog({super.key, required this.completer});
+                }
+              ''',
+                'a|lib/b.dart': '''
+                import 'package:flutter/material.dart';
+
+                class CustomBuilder extends PageRouteBuilder {}
+              '''
+              },
+              outputs: {
+                'a|lib/app.dialogs.dart': r'''
+// ignore_for_file: type=lint
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'dart:ui' as _i2;
+
+import 'package:a/a.dart' as _i4;
+import 'package:a/b.dart' as _i3;
+import 'package:fluorflow/fluorflow.dart' as _i1;
+
+extension Dialogs on _i1.DialogService {
+  Future<(bool?, dynamic)> showMyDialog(
+          {_i2.Color barrierColor = const _i2.Color(0x80000000)}) =>
+      showDialog<(bool?, dynamic)>(
+        barrierColor: barrierColor,
+        dialogBuilder: _i3.CustomBuilder(
+            pageBuilder: (
+          _,
+          __,
+          ___,
+        ) =>
+                _i4.MyDialog(completer: closeDialog)),
+      ).then((r) => (r?.$1, r?.$2));
+}
+'''
+              },
+              reader: await PackageAssetReader.currentIsolate()));
+
+      test(
+          'should throw when custom page is requested, but no page builder is provided.',
+          () async {
+        try {
+          await testBuilder(
+              DialogBuilder(BuilderOptions.empty),
+              {
+                'a|lib/a.dart': '''
+                  import 'package:fluorflow/annotations.dart';
+                  import 'package:fluorflow/fluorflow.dart';
+                  import 'package:flutter/material.dart';
+
+                  @DialogConfig(
+                    routeBuilder: RouteBuilder.custom,
+                  )
+                  class MyDialog extends FluorFlowSimpleDialog {
+                    const MyDialog({super.key, required this.completer});
+                  }
+
+                  class CustomBuilder extends PageRouteBuilder {}
+              '''
+              },
+              reader: await PackageAssetReader.currentIsolate());
+          fail('Should have thrown');
+        } catch (e) {
+          expect(e, isA<InvalidGenerationSourceError>());
+        }
+      });
+
+      for (final (transition, resultBuilder) in RouteBuilder.values
+          .where((t) => t != RouteBuilder.custom)
+          .map((t) => (t, '${t.name.pascalCase}PageRouteBuilder'))) {
+        test(
+            'should use correct page route builder '
+            '($resultBuilder) for transition (${transition.name}).',
+            () async => await testBuilder(
+                DialogBuilder(BuilderOptions.empty),
+                {
+                  'a|lib/a.dart': '''
+                    import 'package:fluorflow/annotations.dart';
+                    import 'package:fluorflow/fluorflow.dart';
+                    import 'package:flutter/material.dart';
+
+                    @DialogConfig(
+                      routeBuilder: RouteBuilder.${transition.name},
+                    )
+                    class MyDialog extends FluorFlowSimpleDialog {
+                      const MyDialog({super.key, required this.completer});
+                    }
+                  ''',
+                },
+                outputs: {
+                  'a|lib/app.dialogs.dart': '''
+// ignore_for_file: type=lint
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'dart:ui' as _i2;
+
+import 'package:a/a.dart' as _i3;
+import 'package:fluorflow/fluorflow.dart' as _i1;
+
+extension Dialogs on _i1.DialogService {
+  Future<(bool?, dynamic)> showMyDialog(
+          {_i2.Color barrierColor = const _i2.Color(0x80000000)}) =>
+      showDialog<(bool?, dynamic)>(
+        barrierColor: barrierColor,
+        dialogBuilder: _i1.$resultBuilder(
+            pageBuilder: (
+          _,
+          __,
+          ___,
+        ) =>
+                _i3.MyDialog(completer: closeDialog)),
+      ).then((r) => (r?.\$1, r?.\$2));
+}
+''',
+                },
+                reader: await PackageAssetReader.currentIsolate()));
+      }
     });
 
     group('with Builder Configuration', () {
-      // TODO
+      test(
+          'should use custom output if configured.',
+          () async => await testBuilder(
+              DialogBuilder(BuilderOptions({
+                'output': 'lib/app/my.dialogs.dart',
+              })),
+              {
+                'a|lib/a.dart': '''
+                import 'package:fluorflow/fluorflow.dart';
+
+                class MyDialog extends FluorFlowSimpleDialog {
+                  const MyDialog({super.key, required this.completer});
+                }
+              '''
+              },
+              outputs: {
+                'a|lib/app/my.dialogs.dart': r'''
+// ignore_for_file: type=lint
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'dart:ui' as _i2;
+
+import 'package:a/a.dart' as _i3;
+import 'package:fluorflow/fluorflow.dart' as _i1;
+
+extension Dialogs on _i1.DialogService {
+  Future<(bool?, dynamic)> showMyDialog(
+          {_i2.Color barrierColor = const _i2.Color(0x80000000)}) =>
+      showDialog<(bool?, dynamic)>(
+        barrierColor: barrierColor,
+        dialogBuilder: _i1.NoTransitionPageRouteBuilder(
+            pageBuilder: (
+          _,
+          __,
+          ___,
+        ) =>
+                _i3.MyDialog(completer: closeDialog)),
+      ).then((r) => (r?.$1, r?.$2));
+}
+'''
+              },
+              reader: await PackageAssetReader.currentIsolate()));
     });
   });
 }
